d010e7a96df7471100720f8e3d493e17d655b4cf
Jos Tung <smilejos.tung@gmail.com>
2023-08-09 14:05:36 +0800

    Initial Commit

---
+++ SyncContacts/SyncContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncContactsApp.swift
+//  SyncContacts
+//
+//  Created by Jos Tung on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct SyncContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
